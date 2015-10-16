a7f1992545ecc2b88e18461118e4efefac950037
Twinkle Chawla <twinkle1chawla@gmail.com>
2015-10-16 22:24:05 +0530

    class

--- class.js
+++ class.js
@@ -7,9 +7,5 @@ this.greeter = greeter;
 alert(this.greeter + ', ' + name)
 	}
 });
-
-// create instance of MyClass, passing "Hello" to the constructor
 var a = new MyClass("Hello");
-
-// call greet method, alerting "Hello, World"
 a.greet("World");
